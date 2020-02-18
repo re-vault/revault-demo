77237da461b38c7644cd1969212f86f96a7385bc
darosior <darosior@protonmail.com>
2020-02-18 16:26:24 +0100

    Add pytest fixtures and testing utilities
    Thanks Christian !

---
+++ tests/fixtures.py
@@ -0,0 +1,98 @@
+"""
+Most of the code here is stolen from C-lightning's test suite. This is surely
+Rusty Russell or Christian Decker who wrote most of this (I'd put some sats on
+cdecker), so credits to them ! (MIT licensed)
+"""
+from utils import BitcoinD
+
+import logging
+import os
+import pytest
+import shutil
+import tempfile
+
+__attempts = {}
+
+
+@pytest.fixture(scope="session")
+def test_base_dir():
+    d = os.getenv("TEST_DIR", "/tmp")
+
+    directory = tempfile.mkdtemp(prefix='ltests-', dir=d)
+    print("Running tests in {}".format(directory))
+
+    yield directory
+
+    if os.listdir(directory) == []:
+        shutil.rmtree(directory)
+
+
+@pytest.fixture
+def directory(request, test_base_dir, test_name):
+    """Return a per-test specific directory.
+
+    This makes a unique test-directory even if a test is rerun multiple times.
+
+    """
+    global __attempts
+    # Auto set value if it isn't in the dict yet
+    __attempts[test_name] = __attempts.get(test_name, 0) + 1
+    directory = os.path.join(test_base_dir,
+                             "{}_{}".format(test_name, __attempts[test_name]))
+    request.node.has_errors = False
+
+    yield directory
+
+    # This uses the status set in conftest.pytest_runtest_makereport to
+    # determine whether we succeeded or failed. Outcome can be None if the
+    # failure occurs during the setup phase, hence the use to getattr instead
+    # of accessing it directly.
+    rep_call = getattr(request.node, 'rep_call', None)
+    outcome = 'passed' if rep_call is None else rep_call.outcome
+    failed = not outcome or request.node.has_errors or outcome != 'passed'
+
+    if not failed:
+        shutil.rmtree(directory)
+    else:
+        logging.debug("Test execution failed, leaving the test directory {}"
+                      " intact.".format(directory))
+
+
+@pytest.fixture
+def test_name(request):
+    yield request.function.__name__
+
+
+@pytest.fixture
+def bitcoind(directory):
+    bitcoind = BitcoinD(bitcoin_dir=directory)
+
+    try:
+        bitcoind.start()
+    except Exception:
+        bitcoind.stop()
+        raise
+
+    info = bitcoind.rpc.getnetworkinfo()
+
+    if info['version'] < 160000:
+        bitcoind.rpc.stop()
+        raise ValueError("bitcoind is too old. At least version 16000"
+                         " (v0.16.0) is needed, current version is {}"
+                         .format(info['version']))
+
+    info = bitcoind.rpc.getblockchaininfo()
+    # Make sure we have some spendable funds
+    if info['blocks'] < 101:
+        bitcoind.generate_block(101 - info['blocks'])
+    elif bitcoind.rpc.getwalletinfo()['balance'] < 1:
+        logging.debug("Insufficient balance, generating 1 block")
+        bitcoind.generate_block(1)
+
+    yield bitcoind
+
+    try:
+        bitcoind.stop()
+    except Exception:
+        bitcoind.proc.kill()
+    bitcoind.proc.wait()
