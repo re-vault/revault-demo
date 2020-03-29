a6556d363fc2fcf59e500e6a1cb3eee5ff3c73e6
darosior <darosior@protonmail.com>
2020-03-29 18:49:29 +0200

    revault: correct the monstruausity which was the cosigning server
    Who the fuck wrote that ??
    
    Seriously. Wtf.

--- revault/cosigning_server.py
+++ revault/cosigning_server.py
@@ -4,7 +4,7 @@ from bitcoin.wallet import CKey
 from bitcoin.core import b2x
 from flask import Flask, jsonify
 
-from .transactions import sign_spend_tx
+from .transactions import create_spend_tx, sign_spend_tx
 
 
 class CosigningServer:
@@ -23,24 +23,29 @@ class CosigningServer:
         self.setup_routes()
 
     def setup_routes(self):
-        @self.server.route("/sign/<string:tx>/<string:pub1>/<string:pub2>/"
-                           "<string:pub3>/<string:pub4>", methods=["GET"])
-        def get_signature(tx, pub1, pub2, pub3, pub4):
-            """Sign a spend transaction. Ugly API because P2(W)SH.
+        @self.server.route("/sign/<string:txid>/<string:pub1>/<string:pub2>/"
+                           "<string:pub3>/<string:pub4>/<string:address>"
+                           "/<int:value>/<int:prev_value>", methods=["GET"])
+        def get_signature(pub1, txid, pub2, pub3, pub4,
+                          address, value, prev_value):
+            """Sign a spend transaction.
 
-            :param tx: The actual spend transaction.
-            :param pub1: The first stakeholder pubkey.
-            :param pub2: The second stakeholder pubkey.
-            :param pub3: The third stakeholder pubkey.
-            :param pub4: The fourth stakeholder pubkey.
+            :param txid: The unvault transaction's txid.
+            :param pub1: The first stakeholder pubkey for this vault.
+            :param pub2: The second stakeholder pubkey for this vault.
+            :param pub3: The third stakeholder pubkey for this vault.
+            :param pub4: The fourth stakeholder pubkey for this vault.
+            :param address: The address to spend to.
+            :param value: The amount to send in sats.
+            :param prev_value: The prevout (unvault) value in sats.
             """
-            # Some sanity checks
-            if b2x(tx.GetTxid()) in self.already_signed \
-                    or len(tx.vin) != 1 or len(tx.vin[0].scriptSig) != 0:
+            if txid in self.already_signed:
                 return jsonify({"sig": None}), 403
 
-            sigs = sign_spend_tx(tx, [self.privkey], [pub1, pub2, pub3, pub4],
-                                 self.pubkey)
+            spend_tx = create_spend_tx(txid, 0, value, address)
+            sigs = sign_spend_tx(spend_tx, [self.privkey],
+                                 [pub1, pub2, pub3, pub4], self.pubkey,
+                                 prev_value)
             return jsonify({"sig": sigs[0]}), 200
 
         @self.server.route("/getpubkey", methods=["GET"])
--- revault/cosigningapi.py
+++ revault/cosigningapi.py
@@ -9,7 +9,7 @@ class CosigningApi:
         """
         self.url = url
 
-    def get_signature(self, tx, pubkeys):
+    def get_signature(self, txid, pubkeys, address, value, prev_value):
         """Request the cosigning server to sign a spend transaction.
 
         :param tx: The actual spend transaction, as str.
@@ -18,12 +18,13 @@ class CosigningApi:
         :return: The signature as bytes.
         """
         r = requests.get("{}/sign/{}/{}/{}/{}/{}"
-                         .format(self.url, tx, *pubkeys))
+                         .format(self.url, txid, *pubkeys, address, value,
+                                 prev_value))
         if r.status_code == 200:
             return bytes.fromhex(r.json()["sig"])
         else:
-            raise Exception("Requesting signature for {} to cosigning"
-                            " server: {}".format(tx, r.text))
+            raise Exception("Requesting signature for spending {} to cosigning"
+                            " server: {}".format(txid, r.text))
 
     def get_pubkey(self):
         """Request the cosigning server's pubkey.
