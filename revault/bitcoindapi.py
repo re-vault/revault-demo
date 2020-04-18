43e4e8470ae96e8335dfc847a0f775edd1d5e255
darosior <darosior@protonmail.com>
2020-04-18 18:51:43 +0200

    revault/bitcoindapi: catch exception in locks...
    Quite a big brainfart here..
    
    Signed-off-by: Antoine Poinsot <darosior@protonmail.com>

--- revault/bitcoindapi.py
+++ revault/bitcoindapi.py
@@ -20,6 +20,22 @@ class BitcoindApi:
         """Close the connection to bitcoind."""
         self.bitcoind.close()
 
+    def call(self, name, *args):
+        """Exception-safe lock handling."""
+        err = None
+        f = getattr(self.bitcoind, name)
+
+        self.bitcoind_lock.acquire()
+        try:
+            res = f(*args)
+        except Exception as e:
+            err = e
+        self.bitcoind_lock.release()
+
+        if err is not None:
+            raise err
+        return res
+
     def importmultiextended(self, xpubs, birthdate, min_index, max_index):
         """Import a 4-of-4 P2WSH descriptor with 4 xpubs (m/0).
 
@@ -29,16 +45,14 @@ class BitcoindApi:
         :param max_index: The derivation index to end scanning for.
         """
         desc = "wsh(multi(4,{}/*,{}/*,{}/*,{}/*))".format(*xpubs)
-        self.bitcoind_lock.acquire()
-        checksum = self.bitcoind.getdescriptorinfo(desc)["checksum"]
-        res = self.bitcoind.importmulti([{
+        checksum = self.call("getdescriptorinfo", desc)["checksum"]
+        res = self.call("importmulti", [{
             "desc": "{}#{}".format(desc, checksum),
             "timestamp": birthdate,
             "range": [min_index, max_index],
             "watchonly": True,
             "label": "revault_vault"
         }])
-        self.bitcoind_lock.release()
         if not res[0]["success"]:
             raise Exception("Failed to import xpubs. "
                             "Descriptor: {}, result: {}"
@@ -46,90 +60,57 @@ class BitcoindApi:
 
     def listunspent(self, minconf=1, maxconf=9999999, addresses=None,
                     unsafe=True):
-        self.bitcoind_lock.acquire()
-        utxos = self.bitcoind.listunspent(minconf, maxconf, addresses, unsafe)
-        self.bitcoind_lock.release()
-        return utxos
+        return self.call("listunspent", minconf, maxconf, addresses, unsafe)
 
     def gettransaction(self, txid):
-        self.bitcoind_lock.acquire()
-        tx = self.bitcoind.gettransaction(txid)
-        self.bitcoind_lock.release()
-        return tx
+        return self.call("gettransaction", txid)
 
     def decoderawtransaction(self, hex):
-        self.bitcoind_lock.acquire()
-        tx = self.bitcoind.decoderawtransaction(hex)
-        self.bitcoind_lock.release()
-        return tx
+        return self.call("decoderawtransaction", hex)
 
     def sendtoaddress(self, address, amount):
-        self.bitcoind_lock.acquire()
-        txid = self.bitcoind.sendtoaddress(address, amount)
-        self.bitcoind_lock.release()
-        return txid
+        return self.call("sendtoaddress", address, amount)
 
     def getrawmempool(self):
-        self.bitcoind_lock.acquire()
-        mempool = self.bitcoind.getrawmempool()
-        self.bitcoind_lock.release()
-        return mempool
+        return self.call("getrawmempool")
 
     def addmultisigaddress(self, num, pubkeys):
-        self.bitcoind_lock.acquire()
-        res = self.bitcoind.addmultisigaddress(num, pubkeys)
-        self.bitcoind_lock.release()
-        return res
+        return self.call("addmultisigaddress", num, pubkeys)
 
     def generatetoaddress(self, num, address):
-        self.bitcoind_lock.acquire()
-        txids = self.bitcoind.generatetoaddress(num, address)
-        self.bitcoind_lock.release()
-        return txids
+        return self.call("generatetoaddress", num, address)
 
     def getnewaddress(self):
-        self.bitcoind_lock.acquire()
-        addr = self.bitcoind.getnewaddress()
-        self.bitcoind_lock.release()
-        return addr
+        return self.call("getnewaddress")
 
     def sendrawtransaction(self, tx):
-        self.bitcoind_lock.acquire()
-        txid = self.bitcoind.sendrawtransaction(tx)
-        self.bitcoind_lock.release()
-        return txid
+        return self.call("sendrawtransaction", tx)
 
     def importaddress(self, address, label, rescan=True):
-        self.bitcoind_lock.acquire()
-        self.bitcoind.importaddress(address, label, rescan)
-        self.bitcoind_lock.release()
+        return self.call("importaddress", address, label, rescan)
 
     def mine(self, txid):
-        while txid not in self.bitcoind.getrawmempool():
+        while txid not in self.getrawmempool():
             time.sleep(0.5)
-        self.bitcoind.generatetoaddress(1, self.bitcoind.getnewaddress())
+        addr = self.call("getnewaddress")
+        self.call("generatetoaddress", 1, addr)
 
     def broadcast_and_mine(self, tx):
         """A routine used in the tests"""
-        self.bitcoind_lock.acquire()
-        txid = self.bitcoind.sendrawtransaction(tx)
+        txid = self.sendrawtransaction(tx)
         self.mine(txid)
-        self.bitcoind_lock.release()
         return txid
 
     def pay_to(self, address, amount):
         """A helper for the functional tests.."""
-        self.bitcoind_lock.acquire()
+        addr = self.call("getnewaddress")
         while self.bitcoind.getbalance() < amount + 1:
-            self.bitcoind.generatetoaddress(1, self.bitcoind.getnewaddress())
-        txid = self.bitcoind.sendtoaddress(address, amount)
+            self.call("generatetoaddress", 1, addr)
+        txid = self.sendtoaddress(address, amount)
         self.mine(txid)
-        self.bitcoind_lock.release()
         return txid
 
     def assertmempoolaccept(self, txs):
         """Helper for sanity checks."""
-        self.bitcoind_lock.acquire()
-        res = self.bitcoind.testmempoolaccept(txs)
-        self.bitcoind_lock.release()
+        res = self.call("testmempoolaccept", txs)
         assert all(tx["allowed"] for tx in res)
